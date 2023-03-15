ad6ede86628b46094125c62f8d866cf5bbabfee1
shado <shadowstakeus28@gmail.com>
2023-03-15 18:09:24 +0300

    Initial commit

---
+++ src/main/java/com/work/checkintegrationfrontendspringbootdatabase/CheckIntegrationFrontendSpringbootDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.work.checkintegrationfrontendspringbootdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckIntegrationFrontendSpringbootDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckIntegrationFrontendSpringbootDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/checkintegrationfrontendspringbootdatabase/CheckIntegrationFrontendSpringbootDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.checkintegrationfrontendspringbootdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckIntegrationFrontendSpringbootDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
